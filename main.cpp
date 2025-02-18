46b77efa2b5ca10c5fa69215e8e776248d0de1b7
Lutho420 <lmboniswa01@gmail.com>
2025-02-19 00:50:26 +0200

    --allow-empty
    just a burner

--- main.cpp
+++ main.cpp
@@ -7,37 +7,26 @@ using namespace std;
 
 int main() {
     StudentManager sm;
-    cout << "Should be an empty list of Student here: " << endl;
-    sm.displayAllStudents();
+    string burner = 
+        "Welcome to my basic SMS (Student Management System) lol\n"
+        "*****************************************************************\n"
+        "   __________      ______         ______         _________  \n"
+        "  /          |    |      \\       /      |       /         | \n"
+        " |     ______|    |       \\     /       |      |     _____| \n"
+        " |     \\          |        \\   /        |      |     \\       \n"
+        " \\      \\         |         \\ /         |      \\      \\      \n"
+        "  \\      \\        |                     |       \\      \\     \n"
+        "   \\      \\       |        |    |       |        \\      \\    \n"
+        "    \\      \\      |        |    |       |         \\      \\   \n"
+        " ____\\      |     |        |    |       |      ____\\      |  \n"
+       "|           |     |        |    |       |     |           |  \n"
+       "|__________/      |________|    |_______|     |__________/    \n"
+       "******************************************************************\n";   
+        
 
-    Student john = Student("John Titor", "JHNTTR002", "Computer Science", 76);
-    Student robot = Student("Mr Robot", "MRRRBT007", "Cybersecurity", 95);
-    Student elliot = Student("Elliot Alderson", "ELTADS004", "Psychology", 43);
-    Student light = Student("Light Yagami", "LGTYGM005", "Ethics", 63);
-    Student eren = Student("Eren Jeager", "ERBYGR008", "Special Relativity", 80);
-
-    sm.addStudent(john);
-    sm.addStudent(robot);
-    sm.addStudent(elliot);
-    sm.addStudent(light);
-    sm.addStudent(eren);
-
-    cout << "\n----    All Students    ----\n";
-    sm.displayAllStudents();
-
-    /*cout << "\nSearching for Student by Name: Elliot" << endl;
-    sm.getStudentByName("Elliot Alderson");
-    cout << "\nSearching for Student by Roll Number: ELTADS004" << endl;
-    sm.getStudentByRollNumber("LGTYGM005");*/
-
-    cout << "\n--- Updating a Student! ---" << endl;
-    sm.updateStudent("MRRRBT007", "Data Science", 72);
-    sm.getStudentByRollNumber("MRRRBT007");
-
-    cout << "\n--- Updating a Student! ---" << endl;
-    sm.deleteStudent("LGTYGM005");
-    sm.deleteStudent("JHNTTR002");
-    sm.displayAllStudents();
+    
+    // TO Do: Implement the main menu
+    cout << burner << endl;
 
     return 0;
 }
